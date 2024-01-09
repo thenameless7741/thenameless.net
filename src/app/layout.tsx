2bbe7845c2e0a54d152d9dea111d20099e581173
thenameless <155974615+thenameless7741@users.noreply.github.com>
2024-01-09 11:54:51 +0700

    feature: set up Sass with global styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from 'next';
 
+import '@/styles/global.scss';
+
 export const metadata: Metadata = {
   title: '[the]nameless',
   description: 'A playspace for experimenting with intelligence amplification.',

7423d136446b4951e0e01b207f78c758ae699acc
thenameless <155974615+thenameless7741@users.noreply.github.com>
2024-01-11 18:16:50 +0700

    feature: set up gray-matter and markdown components

--- src/ui/stella.tsx
+++ src/ui/stella.tsx
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import p from 'path';
 
+import matter from 'gray-matter';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
+import markdown from './markdown';
 import s from './stella.module.scss';
 
 interface StellaProps {
@@ -13,10 +15,14 @@ const Stella = async ({ path }: StellaProps) => {
   const mdxDir = p.join(process.cwd(), 'src', 'mdx');
   const filePath = p.join(mdxDir, path);
   const source = fs.readFileSync(filePath).toString();
+  const { content, data } = matter(source);
 
   return (
     <div className={s.stella}>
-      <MDXRemote source={source} />
+      {Object.entries(data).map(([k, v], i) => (
+        <div key={i}>{`${k}: ${v}`}</div>
+      ))}
+      <MDXRemote components={markdown} source={content} />
     </div>
   );
 };

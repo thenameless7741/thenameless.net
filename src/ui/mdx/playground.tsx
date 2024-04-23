9bcc0818c6ee4270b12ae0cd6d12f5f27070f256
thenameless <155974615+thenameless7741@users.noreply.github.com>
2024-04-23 10:35:53 +0700

    feature: improve playground with read-only parameterized input

--- src/ui/mdx/playground.tsx
+++ src/ui/mdx/playground.tsx
@@ -3,12 +3,13 @@ import s from './playground.module.scss';
 interface Props {
   system?: string;
   user?: string;
-  assistant?: string;
+  assistant?: string | string[]; // array size relative to input
   labels?: {
     system?: string;
     user?: string;
     assistant?: string;
   };
+  input?: Record<string, string> | Record<string, string>[]; // array size relative to assistant
   visible?: {
     system?: boolean;
   };
@@ -17,9 +18,15 @@ interface Props {
 
 const Playground = (p: Props) => {
   return (
-    <div className={[s.playground, p.system ? s['has-system'] : ''].join(' ')}>
+    <div
+      className={[
+        s.playground,
+        p.system ? s['has-system'] : '',
+        p.input ? s['has-input'] : '',
+      ].join(' ')}
+    >
       {!!p.system && (
-        <div className={s.user}>
+        <div className={s.system}>
           <div className={s.label}>{p.labels?.system ?? 'system'}</div>
           <div className={s.content}>{p.system}</div>
         </div>
@@ -30,11 +37,66 @@ const Playground = (p: Props) => {
         <div className={s.content}>{p.user}</div>
       </div>
 
-      <div className={s.assistant}>
-        <div className={s.label}>{p.labels?.assistant ?? 'assistant'}</div>
-        <div className={s.content}>{p.assistant}</div>
+      <div
+        className={s.io}
+        style={{ '--size': p.input?.length } as React.CSSProperties}
+      >
+        {!p.input ? null : Array.isArray(p.input) ? (
+          p.input.map((params, i) => (
+            <Params key={i} label={`input #${i + 1}`} params={params} />
+          ))
+        ) : (
+          <Params params={p.input as Record<string, string>} />
+        )}
+
+        {Array.isArray(p.assistant) ? (
+          p.assistant.map((a, i) => (
+            <Assistant
+              key={i}
+              label={`${p.labels?.assistant ?? 'assistant'}: #${i + 1}`}
+              assistant={a}
+            />
+          ))
+        ) : (
+          <Assistant label={p.labels?.assistant} assistant={p.assistant} />
+        )}
       </div>
     </div>
   );
 };
 export default Playground;
+
+const Params = ({
+  label = 'input',
+  params,
+}: {
+  label?: string;
+  params: Record<string, string>;
+}) => {
+  return (
+    <div className={s.params}>
+      <div className={s.label}>{label}</div>
+
+      <div className={s['params-grid']}>
+        {Object.entries(params).map(([k, v]) => (
+          <>
+            <div className={s['param-key']}>{k}</div>
+            <div className={s['param-value']}>{v}</div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Assistant = ({
+  label = 'assistant',
+  assistant,
+}: { label?: string } & Pick<Props, 'assistant'>) => {
+  return (
+    <div className={s.assistant}>
+      <div className={s.label}>{label}</div>
+      <div className={s.content}>{assistant}</div>
+    </div>
+  );
+};

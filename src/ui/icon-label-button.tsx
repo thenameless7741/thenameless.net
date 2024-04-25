ccdc37a215bc2f3dfb74f4f495efd2b2fd8cb52a
thenameless <155974615+thenameless7741@users.noreply.github.com>
2024-04-25 08:22:18 +0700

    fix: add size and weight props to IconLabelButton

--- src/ui/icon-label-button.tsx
+++ src/ui/icon-label-button.tsx
@@ -1,4 +1,4 @@
-import { Icon as PhosphorIcon } from '@phosphor-icons/react';
+import { Icon as PhosphorIcon, IconProps } from '@phosphor-icons/react';
 import { Button, ButtonProps } from 'react-aria-components';
 
 import { iconProps } from './icon';
@@ -6,13 +6,22 @@ import s from './icon-label-button.module.scss';
 
 type Props = ButtonProps & {
   Icon: PhosphorIcon;
+  size?: IconProps['size'];
+  weight?: IconProps['weight'];
   children: React.ReactNode;
 };
 
-const Component = ({ Icon, children, className, ...props }: Props) => {
+const Component = ({
+  Icon,
+  size = iconProps.size,
+  weight = iconProps.weight,
+  children,
+  className,
+  ...props
+}: Props) => {
   return (
     <Button {...props} className={[s.button, className ?? ''].join(' ')}>
-      <Icon {...iconProps} className={s.icon} />
+      <Icon {...iconProps} className={s.icon} size={size} weight={weight} />
       <span className={s.label}>{children}</span>
     </Button>
   );

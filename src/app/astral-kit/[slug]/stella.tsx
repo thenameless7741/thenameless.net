e4e273ac35d2d52dde3d6077b6bc613129dca903
thenameless <155974615+thenameless7741@users.noreply.github.com>
2024-06-02 21:54:44 +0900

    doc: add images to tut

--- src/app/astral-kit/[slug]/stella.tsx
+++ src/app/astral-kit/[slug]/stella.tsx
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import p from 'path';
 
+import Image from 'next/image';
 import { compileMDX } from 'next-mdx-remote/rsc';
 
 import { Metadata } from '@/types/mdx';
@@ -10,6 +11,7 @@ import { getHeadings } from '@/utils/mdx';
 import Playground from '../playground';
 import Header from './header';
 import Settings from './settings';
+import cs from '@/ui/mdx/components.module.scss';
 import s from '@/ui/stella.module.scss';
 
 interface StellaProps {
@@ -25,6 +27,18 @@ const Stella = async ({ path }: StellaProps) => {
     source,
     components: {
       ...components,
+
+      // override
+      Image: ({ alt, path, width, height }) => (
+        <Image
+          className={cs.image}
+          alt={alt ?? ''}
+          src={`${process.env.NEXT_PUBLIC_CF_R2_BASE_URL}/astral-kit/${path}`}
+          width={width}
+          height={height}
+        />
+      ),
+
       Playground,
       Settings: (props) => <Settings {...props} standalone={false} />,
     },
